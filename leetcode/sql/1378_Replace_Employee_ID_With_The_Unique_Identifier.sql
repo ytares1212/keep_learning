0940b6773517ff518db6f2e75a7604b64456ca1c
Tao Yu <taoyu1212@gmail.com>
2026-03-18 13:13:28 +1100

    daily practise on q1378

---
+++ leetcode/sql/1378_Replace_Employee_ID_With_The_Unique_Identifier.sql
@@ -0,0 +1,30 @@
+-- Table: Employees
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | name          | varchar |
+-- +---------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table contains the id and the name of an employee in a company.
+
+-- Table: EmployeeUNI
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | unique_id     | int     |
+-- +---------------+---------+
+-- (id, unique_id) is the primary key (combination of columns with unique values) for this table.
+-- Each row of this table contains the id and the corresponding unique id of an employee in the company.
+
+-- Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+
+-- Return the result table in any order.
+
+SELECT enui.unique_id, e.name
+FROM Employees e
+LEFT JOIN EmployeeUNI enui
+ON e.id = enui.id
