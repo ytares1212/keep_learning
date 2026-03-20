2a1bf5fc25a851c673f22a2d51867437ec90dec2
Tao Yu <taoyu1212@gmail.com>
2026-03-20 21:26:25 +1100

    daily practise on q151

--- leetcode/array_string/14. Longest Common Prefix.py
+++ leetcode/array_string/14. Longest Common Prefix.py
@@ -14,6 +14,7 @@
 # Input: strs = ["dog","racecar","car"]
 # Output: ""
 # Explanation: There is no common prefix among the input strings.
+from typing import List
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
 
