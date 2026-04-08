4c7ab95bbac07a2537f4aa5d114a586c176cd458
Tao Yu <taoyu1212@gmail.com>
2026-04-08 20:59:22 +1000

    daily practise q1

---
+++ leetcode/Hashmap/1. Two Sum.py
@@ -0,0 +1,35 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+
+ 
+
+# Example 1:
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+# Example 2:
+
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+
+# Example 3:
+
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_idx = {}
+        for idx, num in enumerate(nums):
+            # if there is a number can add up to target
+            if target - num in num_idx:
+                # get the number's index by searching with key(the number itself)
+                return [idx, num_idx[target - num]]
+            # store actual number as key, its index as value
+            # dictionaries are designed for fast lookups by key, not by value
+            num_idx[num] = idx
