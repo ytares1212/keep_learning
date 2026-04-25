c837fa1d90053894ac434f02fa182b6df757fd5c
Tao Yu <taoyu1212@gmail.com>
2026-04-26 09:35:42 +1000

    daily practise q86

---
+++ leetcode/Linked List/86. Partition List.py
@@ -0,0 +1,42 @@
+# Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+# You should preserve the original relative order of the nodes in each of the two partitions.
+
+ 
+
+# Example 1:
+
+# Input: head = [1,4,3,2,5,2], x = 3
+# Output: [1,2,2,4,3,5]
+
+# Example 2:
+
+# Input: head = [2,1], x = 2
+# Output: [1,2]
+from typing import Optional
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        less_head = ListNode(0)
+        greater_head = ListNode(0)
+        less = less_head
+        greater = greater_head
+
+        while head:
+            if head.val < x:
+                less.next = head
+                less = less.next
+            else:
+                greater.next = head
+                greater = greater.next
+            head = head.next
+
+        # Connect the two partitions
+        greater.next = None  # Important to avoid cycle
+        less.next = greater_head.next
+
+        return less_head.next
