c8edcc805d9b59dbd0a459b6f484297b7cb82a3f
Tao Yu <taoyu1212@gmail.com>
2026-05-18 11:01:19 +1000

    daily practise q230

---
+++ leetcode/Binary Tree General/230. Kth Smallest Element in a BST.py
@@ -0,0 +1,29 @@
+# Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+# Example 1:
+# Input: root = [3,1,4,null,2], k = 1
+# Output: 1
+
+# Example 2:
+# Input: root = [5,3,6,2,4,null,null,1], k = 3
+# Output: 3
+from typing import Optional
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        if not root:
+            return 
+        node_list = []
+        def inorder(node):
+            if not node:
+                return 
+            inorder(node.left)
+            node_list.append(node.val)
+            inorder(node.right)
+        inorder(root)
+        return node_list[k-1]
