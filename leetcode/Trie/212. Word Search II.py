e8a57e7356ca77a365a95f7349ac996309ffb73c
Tao Yu <taoyu1212@gmail.com>
2026-06-03 16:23:00 +1000

    daily practise q212

--- leetcode/Trie/212. Word Search II.py
+++ leetcode/Trie/212. Word Search II.py
@@ -1,15 +1,47 @@
 # Given an m x n board of characters and a list of strings words, return all words on the board.
-
 # Each word must be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once in a word.
 
- 
-
 # Example 1:
-
 # Input: board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]], words = ["oath","pea","eat","rain"]
 # Output: ["eat","oath"]
 
 # Example 2:
-
 # Input: board = [["a","b"],["c","d"]], words = ["abcb"]
 # Output: []
+from typing import List
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        # Build a trie from the list of words
+        trie = {}
+        for word in words:
+            node = trie
+            for c in word:
+                if c not in node:
+                    node[c] = {}
+                node = node[c]
+            node['#'] = word
+
+        result = []
+
+        def dfs(i, j, node):
+            c = board[i][j]
+            if c not in node:
+                return
+
+            node = node[c]
+            if '#' in node:
+                result.append(node['#'])
+                del node['#']
+
+            board[i][j] = '#'
+            for di, dj in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                ni, nj = i + di, j + dj
+                if 0 <= ni < len(board) and 0 <= nj < len(board[0]) and board[ni][nj] != '#':
+                    dfs(ni, nj, node)
+            board[i][j] = c
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                dfs(i, j, trie)
+
+        return result
