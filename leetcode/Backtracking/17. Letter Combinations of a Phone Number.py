125dd572b56096accf8736290510529ce4471ee3
Tao Yu <taoyu1212@gmail.com>
2026-06-04 21:28:33 +1000

    daily practise q17

--- leetcode/Backtracking/17. Letter Combinations of a Phone Number.py
+++ leetcode/Backtracking/17. Letter Combinations of a Phone Number.py
@@ -1,15 +1,41 @@
 # Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
-
 # A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
 
- 
-
 # Example 1:
-
 # Input: digits = "23"
 # Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
 
 # Example 2:
-
 # Input: digits = "2"
 # Output: ["a","b","c"]
+from typing import List
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+
+        digit_to_char = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+
+        result = []
+
+        def backtrack(index, path):
+            if index == len(digits):
+                result.append(''.join(path))
+                return
+
+            for char in digit_to_char[digits[index]]:
+                path.append(char)
+                backtrack(index + 1, path)
+                path.pop()
+
+        backtrack(0, [])
+        return result
